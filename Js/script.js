a7275b0f1c2490fd4cb54fcdd5d42feb3519a8e3
Olliemint <furywrld5@gmail.com>
2022-03-21 00:57:41 +0300

    adding buttons and scripts

--- Js/script.js
+++ Js/script.js
@@ -81,6 +81,74 @@ console.log(myarray);
 
 })
 
+// add order script
+let button5 = document.getElementById("btn5");
+
+button5.addEventListener("click", (e) =>{
+    e.preventDefault();
+
+    // pizza selection
+    var pizzaPicked = document.getElementById("pizzacost");
+    var Pizza = parseInt(pizzaPicked.options[pizzaPicked.selectedIndex].value);
+
+    var Pizzalabel = document.getElementById("pizzacost");
+    var Pizzasize = Pizzalabel.options[Pizzalabel.selectedIndex].text;
+    
+    
+
+    // var strUser = e.options[e.selectedIndex].value;
+
+    //    crust selection
+    var crustPicked = document.getElementById("mycrust");
+    var Crust = parseInt(crustPicked.options[crustPicked.selectedIndex].value);
+
+    var Crustlabel = document.getElementById("mycrust");
+    var Crustsize = Crustlabel.options[Crustlabel.selectedIndex].text;
+
+
+    // topping selection
+    var toppingPicked = document.getElementById("mytopping");
+    var Topping = parseInt(toppingPicked.options[toppingPicked.selectedIndex].value);
+
+    var Toppinglabel = document.getElementById("mytopping");
+    var Toppingsize = Toppinglabel.options[Toppinglabel.selectedIndex].text;
+
+    
+
+
+    
+    let orderTotal = new myorder(Pizza, Crust, Topping);
+
+    
+
+    
+
+    
+
+  
+
+//     myarray.push(orderTotal.Fullorder());
+//     console.log(myarray);
+
+
+    var node = document.createElement('li');
+node.appendChild(document.createTextNode(`You ordered ${Pizzasize} Pizza with ${Crustsize} crust & ${Toppingsize} topping @${orderTotal.Fullorder()}Ksh`));
+ 
+document.getElementById("ul").appendChild(node);
+myarray.push(orderTotal.Fullorder())
+
+
+console.log(myarray);
+
+   
+        
+    
+    
+
+
+})
+
+
 
 // book now script
 let button4 = document.getElementById("btn4");
